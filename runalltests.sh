f74b72a242a649ab5005bdb39c598b20da00a2cc
Michal Kwiatkowski <constant.beta@gmail.com>
2010-03-19 16:34:49 +0100

    Run tests in non-verbose mode.

--- runalltests.sh
+++ runalltests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-nosetests-2.3 -v --with-doctest
-nosetests-2.4 -v --with-doctest
-nosetests-2.5 -v --with-doctest
-nosetests-2.6 -v --with-doctest
+nosetests-2.3 --with-doctest
+nosetests-2.4 --with-doctest
+nosetests-2.5 --with-doctest
+nosetests-2.6 --with-doctest
